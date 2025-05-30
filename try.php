22993cb080ecff364defadde1fd46e4a6c8f5ec9
KATY180525 <katyac15210@gmail.com>
2025-05-30 21:08:57 +0800

    Add files via upload

---
+++ try.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>TRYING PHP</title>
+
+<meta charset="UTF-8">
+<meta name="author" content="NGHAUYING">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Boldonse&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+
+
+<style>
+body {
+  background-image: url("../I D K.png");
+  background-attachment: fixed;
+  background-size: cover;
+  font-family: "Roboto Slab", serif;
+
+}
+
+p {
+  font-size: 20px;
+  color: rgb(130, 255, 220)
+
+}
+
+h1{
+  font-size: 40px;
+  color: #ffffff
+}
+
+li{
+  font-size: 15px;
+  color:#f8b5af;
+  font-family: Boldonse;
+}
+
+form{
+  font-size: 20px;
+  color: #ffffff
+}
+
+
+a:link {color: #276da6;}
+
+/* visited link */
+a:visited {color: #27b5af;}
+
+/* mouse over link */
+a:hover {color: #761655;}
+
+/* selected link */
+a:active {color: #276d55;}
+
+</style>
+<script></script>
+</head>
+<body>
+
+</body>
+
+</html>
